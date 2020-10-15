57a53f58fc724e76a4773d7d20ed9915284edc68
Paul Hollyer <paul@hollyer.me.uk>
2020-10-15 11:46:08 +0100

    add button and paragraph functions

--- example/chat_room/assets/elm/src/Page.elm
+++ example/chat_room/assets/elm/src/Page.elm
@@ -1,7 +1,10 @@
 module Page exposing
-    ( Page(..)
+    ( Button
+    , Page(..)
+    , button
     , container
     , header
+    , paragraph
     , view
     )
 
@@ -10,7 +13,9 @@ import Colors.Opaque as Color
 import Element as El exposing (Element)
 import Element.Background as Background
 import Element.Border as Border
+import Element.Events as Event
 import Element.Font as Font
+import Element.Input as Input
 import Html exposing (Html)
 import Phoenix
 
@@ -68,3 +73,65 @@ header title =
         , Font.size 40
         ]
         (El.text title)
+
+
+paragraph : List (Element msg) -> Element msg
+paragraph content =
+    El.paragraph
+        []
+        content
+
+
+type alias Button a msg =
+    { enabled : Bool
+    , label : String
+    , example : a
+    , onPress : a -> msg
+    , onEnter : a -> msg
+    }
+
+
+button : Button a msg -> Element msg
+button { enabled, label, example, onPress, onEnter } =
+    Input.button
+        [ Background.color <|
+            if enabled then
+                Color.darkseagreen
+
+            else
+                Color.grey
+        , Border.rounded 10
+        , El.padding 10
+        , El.mouseOver <|
+            if enabled then
+                [ Border.shadow
+                    { size = 1
+                    , blur = 2
+                    , color = Color.seagreen
+                    , offset = ( 0, 0 )
+                    }
+                , Font.size 31
+                ]
+
+            else
+                []
+        , Event.onMouseEnter <| onEnter example
+        , Font.color <|
+            if enabled then
+                Color.darkolivegreen
+
+            else
+                Color.darkgrey
+        , Font.size 30
+        ]
+        { label =
+            El.el
+                []
+                (El.text label)
+        , onPress =
+            if enabled then
+                Just (onPress example)
+
+            else
+                Nothing
+        }
