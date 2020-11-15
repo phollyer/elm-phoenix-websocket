d17b1e5f535efb80d1265e0104a560eac79d733d
Paul Hollyer <paul@hollyer.me.uk>
2020-11-15 18:25:27 +0000

    refactoring Template into View

--- example/chat_room/assets/elm/src/View/LobbyMembers.elm
+++ example/chat_room/assets/elm/src/View/LobbyMembers.elm
@@ -5,8 +5,11 @@ module View.LobbyMembers exposing
     )
 
 import Device exposing (Device)
-import Element exposing (Element)
-import Template.LobbyMembers.PhonePortrait as PhonePortrait
+import Element as El exposing (Element)
+
+
+
+{- Model -}
 
 
 type Config
@@ -25,11 +28,32 @@ init =
         { members = [] }
 
 
-view : Device -> Config -> Element msg
-view { class, orientation } (Config config) =
-    PhonePortrait.view config
-
-
 members : List User -> Config -> Config
 members users (Config config) =
     Config { config | members = users }
+
+
+
+{- View -}
+
+
+view : Device -> Config -> Element msg
+view _ (Config config) =
+    El.column
+        [ El.width El.fill
+        , El.clipY
+        , El.paddingEach
+            { left = 0
+            , top = 0
+            , right = 0
+            , bottom = 10
+            }
+        , El.scrollbarY
+        , El.spacing 10
+        ]
+    <|
+        List.map
+            (\member ->
+                El.text member.username
+            )
+            config.members
