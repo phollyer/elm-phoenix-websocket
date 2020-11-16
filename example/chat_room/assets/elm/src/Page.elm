106d90683d99a67ef10f190a67cfa007888792f0
Paul Hollyer <paul@hollyer.me.uk>
2020-11-16 19:36:56 +0000

    refactor to remove Templates and View

--- example/chat_room/assets/elm/src/Page.elm
+++ example/chat_room/assets/elm/src/Page.elm
@@ -4,9 +4,12 @@ module Page exposing
     )
 
 import Browser exposing (Document)
+import Colors.Opaque as Color
 import Device exposing (Device)
-import Element exposing (Element)
-import View.Page as Page
+import Element as El exposing (Attribute, DeviceClass(..), Element)
+import Element.Background as Background
+import Element.Border as Border
+import Html exposing (Html)
 
 
 {-| Pages with content
@@ -25,8 +28,93 @@ view : Device -> { title : String, content : Element msg } -> Document msg
 view device { title, content } =
     { title = title ++ " - Elm Phoenix Websocket Example"
     , body =
-        [ Page.init
-            |> Page.body content
-            |> Page.view device
+        [ El.layout
+            [ padding device
+            , Background.color Color.aliceblue
+            , El.height El.fill
+            , El.width El.fill
+            ]
+          <|
+            El.el
+                [ roundedBorder device
+                , shadow device
+                , paddingXY device
+                , Background.color Color.skyblue
+                , El.height El.fill
+                , El.width El.fill
+                , El.clip
+                ]
+                content
         ]
     }
+
+
+
+{- Attributes -}
+
+
+padding : Device -> Attribute msg
+padding { class } =
+    case class of
+        Phone ->
+            El.padding 10
+
+        Tablet ->
+            El.padding 20
+
+        _ ->
+            El.padding 30
+
+
+paddingXY : Device -> Attribute msg
+paddingXY { class } =
+    case class of
+        Phone ->
+            El.paddingXY 10 0
+
+        Tablet ->
+            El.paddingXY 20 0
+
+        _ ->
+            El.paddingXY 30 0
+
+
+roundedBorder : Device -> Attribute msg
+roundedBorder { class } =
+    case class of
+        Phone ->
+            Border.rounded 10
+
+        Tablet ->
+            Border.rounded 20
+
+        _ ->
+            Border.rounded 30
+
+
+shadow : Device -> Attribute msg
+shadow { class } =
+    case class of
+        Phone ->
+            Border.shadow
+                { size = 2
+                , blur = 5
+                , color = Color.lightblue
+                , offset = ( 0, 0 )
+                }
+
+        Tablet ->
+            Border.shadow
+                { size = 3
+                , blur = 10
+                , color = Color.lightblue
+                , offset = ( 0, 0 )
+                }
+
+        _ ->
+            Border.shadow
+                { size = 5
+                , blur = 20
+                , color = Color.lightblue
+                , offset = ( 0, 0 )
+                }
