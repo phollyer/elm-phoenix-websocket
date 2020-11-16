967636bf7f755f6f48118aadba7d60158f1a4d22
Paul Hollyer <paul@hollyer.me.uk>
2020-11-16 17:19:47 +0000

    refactor Templates into View

--- example/chat_room/assets/elm/src/View/FeedbackInfo.elm
+++ example/chat_room/assets/elm/src/View/FeedbackInfo.elm
@@ -9,11 +9,15 @@ module View.FeedbackInfo exposing
     , view
     )
 
+import Colors.Opaque as Color
 import Device exposing (Device)
-import Element exposing (DeviceClass(..), Element, Orientation(..))
+import Element as El exposing (Attribute, DeviceClass(..), Element, Orientation(..))
+import Element.Font as Font
 import Json.Encode as JE exposing (Value)
-import Template.FeedbackInfo.PhoneLandscape as PhoneLandscape
-import Template.FeedbackInfo.PhonePortrait as PhonePortrait
+
+
+
+{- Model -}
 
 
 type Config msg
@@ -35,16 +39,6 @@ init =
         , joinRef = Nothing
         , ref = Nothing
         }
-
-
-view : Device -> Config msg -> Element msg
-view { class, orientation } (Config config) =
-    case ( class, orientation ) of
-        ( Phone, Portrait ) ->
-            PhonePortrait.view config
-
-        _ ->
-            PhoneLandscape.view config
 
 
 topic : String -> Config msg -> Config msg
@@ -78,3 +72,81 @@ joinRef joinRef_ (Config config) =
 ref : Maybe String -> Config msg -> Config msg
 ref ref_ (Config config) =
     Config { config | ref = ref_ }
+
+
+
+{- View -}
+
+
+view : Device -> Config msg -> Element msg
+view device (Config config) =
+    El.column
+        [ El.width El.fill
+        , El.alignLeft
+        , El.paddingEach
+            { left = 0
+            , top = 0
+            , right = 0
+            , bottom = 10
+            }
+        , El.spacing 10
+        , Font.family [ Font.typeface "Roboto Mono" ]
+        ]
+        [ field device "Topic: " config.topic
+        , maybe (field device) "Event: " config.event
+        , field device "Payload: " (JE.encode 2 config.payload)
+        , maybe (field device) "Join Ref: " config.joinRef
+        , maybe (field device) "Ref: " config.ref
+        ]
+
+
+field : Device -> String -> String -> Element msg
+field device label topic_ =
+    El.wrappedRow
+        [ fontSize device
+        , spacing device
+        , El.width El.fill
+        ]
+        [ El.el
+            [ El.alignTop
+            , Font.color Color.darkslateblue
+            ]
+            (El.text label)
+        , El.el
+            [ Font.color Color.black ]
+            (El.text topic_)
+        ]
+
+
+maybe : (String -> String -> Element msg) -> String -> Maybe String -> Element msg
+maybe toField label maybeValue =
+    case maybeValue of
+        Just value ->
+            toField label value
+
+        Nothing ->
+            El.none
+
+
+
+{- Attributes -}
+
+
+fontSize : Device -> Attribute msg
+fontSize { class } =
+    case class of
+        Phone ->
+            Font.size 12
+
+        _ ->
+            Font.size 14
+
+
+spacing : Device -> Attribute msg
+spacing { class, orientation } =
+    case ( class, orientation ) of
+        ( Phone, Portrait ) ->
+            El.spacing 5
+
+        _ ->
+            El.spacing 10
