b478618a90970c62254d097b042385321378ded2
Paul Hollyer <paul@hollyer.me.uk>
2020-11-16 12:39:04 +0000

    refactor Templates into View

--- example/chat_room/assets/elm/src/View/Example.elm
+++ example/chat_room/assets/elm/src/View/Example.elm
@@ -1,29 +1,23 @@
 module View.Example exposing
-    ( Config, init
+    ( Config
+    , controls
+    , description
+    , feedback
+    , id
+    , init
     , view
-    , id, description, controls, feedback
     )
 
-{-| This module is intended to enable building up an example with pipelines and
-then passing off the example config to the chosen template - currently there is
-only one.
-
-@docs Config, init
-
-@docs Template, view
-
-@docs id, introduction, menu, description, controls, remoteControls, feedback, info
-
--}
-
+import Colors.Opaque as Color
 import Device exposing (Device)
-import Element as El exposing (DeviceClass(..), Element, Orientation(..))
-import Template.Example.PhoneLandscape as PhoneLandscape
-import Template.Example.PhonePortrait as PhonePortrait
-import Template.Example.Tablet as Tablet
+import Element as El exposing (Attribute, DeviceClass(..), Element, Orientation(..))
+import Element.Font as Font
 
 
-{-| -}
+
+{- Model -}
+
+
 type Config msg
     = Config
         { id : Maybe String
@@ -33,7 +27,6 @@ type Config msg
         }
 
 
-{-| -}
 init : Config msg
 init =
     Config
@@ -44,39 +37,116 @@ init =
         }
 
 
-{-| -}
-view : Device -> Config msg -> Element msg
-view { class, orientation } (Config config) =
-    case ( class, orientation ) of
-        ( Phone, Portrait ) ->
-            PhonePortrait.view config
-
-        ( Phone, Landscape ) ->
-            PhoneLandscape.view config
-
-        _ ->
-            Tablet.view config
-
-
-{-| -}
 controls : Element msg -> Config msg -> Config msg
 controls cntrls (Config config) =
     Config { config | controls = cntrls }
 
 
-{-| -}
 description : List (List (Element msg)) -> Config msg -> Config msg
 description desc (Config config) =
     Config { config | description = desc }
 
 
-{-| -}
 feedback : Element msg -> Config msg -> Config msg
 feedback feedback_ (Config config) =
     Config { config | feedback = feedback_ }
 
 
-{-| -}
 id : Maybe String -> Config msg -> Config msg
-id maybeId (Config config) =
-    Config { config | id = maybeId }
+id maybeId_ (Config config) =
+    Config { config | id = maybeId_ }
+
+
+
+{- View -}
+
+
+view : Device -> Config msg -> Element msg
+view device (Config config) =
+    El.column
+        [ fontSize device
+        , El.height El.fill
+        , El.width El.fill
+        , El.spacing 10
+        , El.paddingEach
+            { left = 0
+            , top = 0
+            , right = 0
+            , bottom = 10
+            }
+        ]
+        [ descriptionView config.description
+        , maybeId config.id
+        , controlsView config.controls
+        , config.feedback
+        ]
+
+
+
+{- Description -}
+
+
+descriptionView : List (List (Element msg)) -> Element msg
+descriptionView content =
+    El.column
+        [ El.spacing 12
+        , Font.color Color.darkslateblue
+        , Font.justify
+        , Font.family
+            [ Font.typeface "Varela Round" ]
+        , El.width El.fill
+        ]
+    <|
+        List.map
+            (\paragraph ->
+                El.paragraph
+                    [ El.width El.fill ]
+                    paragraph
+            )
+            content
+
+
+
+{- Example ID -}
+
+
+maybeId : Maybe String -> Element msg
+maybeId maybeId_ =
+    case maybeId_ of
+        Nothing ->
+            El.none
+
+        Just id_ ->
+            El.paragraph
+                [ Font.center
+                , Font.family
+                    [ Font.typeface "Varela Round" ]
+                ]
+                [ El.el [ Font.color Color.lavender ] (El.text "Example ID: ")
+                , El.el [ Font.color Color.powderblue ] (El.text id_)
+                ]
+
+
+
+{- Controls -}
+
+
+controlsView : Element msg -> Element msg
+controlsView controls_ =
+    El.el
+        [ El.width El.fill ]
+        controls_
+
+
+
+{- Attributes -}
+
+
+fontSize : Device -> Attribute msg
+fontSize { class } =
+    case class of
+        Phone ->
+            Font.size 14
+
+        _ ->
+            Font.size 18
