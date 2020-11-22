6030cf58119b0b47ec2f83b7c730b85c128d7f73
Paul Hollyer <paul@hollyer.me.uk>
2020-11-22 12:24:28 +0000

    update dependencies

--- example/mix.exs
+++ example/mix.exs
@@ -41,7 +41,9 @@ defmodule ElmPhoenixWebSocketExample.MixProject do
       {:telemetry_poller, "~> 0.4"},
       {:gettext, "~> 0.11"},
       {:jason, "~> 1.0"},
-      {:plug_cowboy, "~> 2.0"}
+      {:plug_cowboy, "~> 2.0"},
+      {:distillery, "~> 2.1"},
+      {:edeliver, "~> 1.8"}
     ]
   end
 
